6f3e3fbf75a78c5a4c4a75768551dee6443c73fa
chimurai <655241+chimurai@users.noreply.github.com>
2020-12-13 19:40:59 +0100

    docs: highlight cmd in messages (#141)

--- tests/requirements.config.js
+++ tests/requirements.config.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk');
+
 module.exports = {
   software: {
     // java: '>= 1.8.x',
@@ -6,13 +8,15 @@ module.exports = {
     npm: '>= 6.x',
     yarn: {
       semver: '1.16.x',
-      updateMessage: `Outdated 'yarn' found.\nRun 'brew upgrade yarn' to update.`,
+      updateMessage: `Outdated 'yarn' found.\nRun '${chalk.cyan('brew upgrade yarn')}' to update.`,
     },
     mvn: '^3.x',
     nginx: {
       semver: '^6.x',
       optional: true,
-      installMessage: `This project is configured for NGINX but 'nginx' was not found on your path.\nRun 'brew install nginx' to install.`,
+      installMessage: `This project is configured for NGINX, but 'nginx' was not found on your path.\nRun '${chalk.cyan(
+        'brew install nginx'
+      )}' to install.`,
     },
     httpd: {
       semver: '^2.x',
