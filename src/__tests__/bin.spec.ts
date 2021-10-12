244f5099ec4fbbca7b3bfea1c14d8ee375371a8e
chimurai <655241+chimurai@users.noreply.github.com>
2021-10-12 22:34:13 +0200

    feat: custom checks

--- src/__tests__/bin.spec.ts
+++ src/__tests__/bin.spec.ts
@@ -5,10 +5,12 @@ import { exec } from '../bin';
 describe('bin', () => {
   let logSpy;
   let debugSpy;
+  let errorSpy;
 
   beforeEach(() => {
     logSpy = jest.spyOn(console, 'log');
     debugSpy = jest.spyOn(console, 'debug');
+    errorSpy = jest.spyOn(console, 'error');
     jest.resetAllMocks();
   });
 
@@ -36,13 +38,53 @@ describe('bin', () => {
     expect(debugSpy).toHaveBeenCalled();
   });
 
-  it('should scaffold with --init', async () => {
-    const filePath = './requirements.config.mjs';
+  describe('Configuration scaffold', () => {
+    it('should scaffold with --init', async () => {
+      const filePath = './requirements.config.mjs';
 
-    await exec({ init: true });
-    expect(fs.existsSync(filePath)).toBe(true);
+      await exec({ init: true });
+      expect(fs.existsSync(filePath)).toBe(true);
 
-    // clean up
-    fs.unlinkSync(filePath);
+      // clean up
+      fs.unlinkSync(filePath);
+    });
+  });
+
+  describe('Software Checks', () => {
+    it('should execute ok tests', async () => {
+      await exec({ config: './tests/ok_every.config.mjs' });
+      expect(logSpy).toHaveBeenNthCalledWith(1, '🔍  Checking software requirements...');
+      expect(logSpy).toHaveBeenNthCalledWith(3, '✅  All is well!');
+    });
+
+    it('should execute nok tests', async () => {
+      await expect(exec({ config: './tests/ok_some.config.mjs' })).rejects.toMatchInlineSnapshot(
+        `[Error: ❌  Not all requirements are satisfied]`
+      );
+    });
+
+    it('should execute nok --force tests', async () => {
+      await exec({ config: './tests/ok_some.config.mjs', force: true });
+      expect(logSpy).toHaveBeenNthCalledWith(3, '⚠️  Not all requirements are satisfied (--force)');
+    });
+
+    it('should print debug data with --debug', async () => {
+      await exec({ config: './tests/ok_every.config.mjs', debug: true });
+      expect(debugSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('Custom Checks', () => {
+    it('should execute nok tests', async () => {
+      await expect(
+        exec({ config: './tests/custom-check-always_fail.mjs' })
+      ).rejects.toMatchInlineSnapshot(`[Error: ❌  Not all custom requirements are satisfied]`);
+    });
+
+    it('should not reject with --force', async () => {
+      await expect(
+        exec({ config: './tests/custom-check-always_fail.mjs', force: true })
+      ).resolves.toBe(undefined);
+    });
   });
 });
